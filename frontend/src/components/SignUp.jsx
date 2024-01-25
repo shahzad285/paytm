66605947984f37fd5a27da8f910c5a858f81ae92
Shahzad Ahamad <ShahzadAhamad285@gmail.com>
2024-01-25 11:51:10 +0530

    Signup page

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,8 +1,26 @@
-export function SignUp()
-{
-return <>
-<h1 className="text-3xl font-bold underline bg-gray-600">
-      Hello world!
-    </h1>
-</>
+export function SignUp() {
+  return <>
+    <div className="min-h-screen bg-custom-color flex justify-center items-center">
+      <div className="rounded bg-slate-50 p-4 ">
+        <div className="flex flex-col items-center">
+          <div className="text-2xl font-bold items-center">Sign Up</div>
+          <div className="text-gray-500  text-xs">Enter your information to create an account</div>
+        </div>
+        <div className="flex flex-col justify-start text-xs">
+          <label className="font-bold" htmlFor="">First Name</label>
+          <input className="rounded border border-gray-500 p-1 my-2" type="text" placeholder="John" />
+          <label className="font-bold" htmlFor="">Last Name</label>
+          <input className="rounded border border-gray-500 p-1 my-2" type="text" placeholder="Doe" />
+          <label className="font-bold" htmlFor="">Email</label>
+          <input className="rounded border border-gray-500 p-1 my-2" type="text" placeholder="Johndoe@example.com" />
+          <label className="font-bold" htmlFor="">Password</label>
+          <input className="rounded border border-gray-500 p-1 my-2" type="password" />
+          <button className="rounded bg-slate-900 text-slate-50 p-1">Sign Up</button>
+        </div>
+
+        <label className="font-bold  text-xs">Already have an account? Login</label>
+
+      </div>
+    </div>
+  </>
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'custom-color': '#7e7f7e', // Replace with your custom color code
+      },     
+    },
   },
   plugins: [],
 }
